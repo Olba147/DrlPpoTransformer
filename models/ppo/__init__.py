47a4be26a026d140fb46bbd913bcaffb026b11ec
Olba147 <88975107+Olba147@users.noreply.github.com>
2026-01-27 23:30:10 +0200

    Revert "Add PPO actor-critic module with tanh-squashed Normal policy utilities"

--- models/ppo/__init__.py
+++
@@ -1,9 +0,0 @@
-from .actor_critic import PPOActor, PPOActorCritic, PPOCritic, tanh_normal_log_prob, tanh_normal_sample
-
-__all__ = [
-    "PPOActor",
-    "PPOCritic",
-    "PPOActorCritic",
-    "tanh_normal_sample",
-    "tanh_normal_log_prob",
-]
